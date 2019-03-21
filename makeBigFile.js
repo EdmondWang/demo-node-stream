a3aada23a441bb76b4c8cb29f4c38cabc728f7f5
Edmond Wang <644568491@qq.com>
2019-03-21 13:48:08 +0800

    Refine content of big file

--- makeBigFile.js
+++ makeBigFile.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const file = fs.createWriteStream('./dist/big.file');
 
 for (var i = 0; i < 1e6; i++) {
-    file.write(i.toString() + '\n');
+    file.write('A stream is an abstract interface for working with streaming data in Node.js. The stream module provides a base API that makes it easy to build objects that implement the stream interface.');
 }
 file.end();
